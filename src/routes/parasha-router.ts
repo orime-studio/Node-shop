17866d8a726b7268ed3b7f6c3182fe30ce4f5789
TamamTamar <tamar.tamam770@gmail.com>
2024-11-18 11:45:24 +0200

    e

--- src/routes/parasha-router.ts
+++ src/routes/parasha-router.ts
@@ -24,7 +24,7 @@ router.post("/", isAdmin, upload.single("image"), async (req, res, next) => {
   
       // הוספת ה-userId לנתונים של הפרשה
 
-      const imageUrl = req.file `https://node-tandt-shop.onrender.com/uploads/${req.file.filename}`;
+      const imageUrl = req.file ? `https://node-tandt-shop.onrender.com/uploads/${req.file.filename}` : req.body.imageUrl;
       res.json({ imageUrl })
       const parashaData = { 
         ...req.body, 
@@ -34,7 +34,7 @@ router.post("/", isAdmin, upload.single("image"), async (req, res, next) => {
       const result = await parashaService.createParasha(parashaData);
       res.status(201).json(result);
     } catch (e) {
-      console.error(e);
+      console.log(e);
       next(e);
     }
   });
