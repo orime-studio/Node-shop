a78e61ee9253a9f004fa9f0b8b8f5e66e2169648
TamamTamar <tamar.tamam770@gmail.com>
2024-11-19 13:19:28 +0200

    gbh

--- src/routes/new-parasha-router.ts
+++ src/routes/new-parasha-router.ts
@@ -9,31 +9,33 @@ const router = Router();
 // POST /parasha - Create a new Parasha
 // Add new parasha
 router.post("/", isAdmin, upload.single("image"), async (req, res, next) => {
-    try {
-      console.log("Payload:", req.payload); // הוספת דיבאג
-      if (!req.payload) {
-        throw new Error("Invalid token");
-      }
-  
-      // אם יש קובץ תמונה
-      const imageUrl = req.file ? `https://node-tandt-shop.onrender.com/uploads/${req.file.filename}` : null;
-  
-      // נתונים של הפרשה
-      const parashaData = { 
-        ...req.body, 
-        images: imageUrl ? [{ url: imageUrl, alt: req.body.alt }] : [],  // אם יש תמונה, הוסף אותה
-        userId: req.payload._id,  // הוספת מזהה המשתמש
-      };
-  
-      // יצירת הפרשה והחיסול
-      const result = await parashaService.createParasha(parashaData);
-      
-      // החזרת תוצאה ללקוח
-      res.status(201).json(result);
-    } catch (e) {
-      next(e);
+  try {
+    console.log("Payload:", req.payload); // הוספת דיבאג
+    if (!req.payload) {
+      throw new Error("Invalid token");
     }
-  });
-  
+
+    // יצירת URL לתמונה
+    const imageUrl = `https://node-tandt-shop.onrender.com/uploads/${req.file.filename}`;
+    
+    // שליחת התמונה ונתונים נוספים כתגובה לפני היצירה
+    res.json({ imageUrl });
+
+    // יצירת אובייקט פרשה עם הנתונים שנשלחו בבקשה
+    const parashaData = { 
+      ...req.body, 
+      images: [{ url: imageUrl, alt: req.body.alt }],  // הוספת URL לתמונה וה-alt
+     
+    };
+
+    // יצירת פרשה בבסיס הנתונים
+    const result = await parashaService.createParasha(parashaData);
+
+    // החזרת תוצאה מוצלחת ללקוח
+    res.status(201).json(result);
+  } catch (e) {
+    next(e);  // טיפול בשגיאות
+  }
+});
 
 export { router as newParashaRouter };
