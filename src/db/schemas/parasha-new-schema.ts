7c456e542c91a3430fab8c48a1daecd1c7c0196d
TamamTamar <tamar.tamam770@gmail.com>
2024-11-19 12:53:32 +0200

    y

--- src/db/schemas/parasha-new-schema.ts
+++ src/db/schemas/parasha-new-schema.ts
@@ -2,17 +2,17 @@ import { Schema } from "mongoose";
 import imageSchema from "./image-schema";
 
 const ParashPageSchema = new Schema({
-    title: { type: String, required: false },
-    text: { type: String, required: false },
+    title: { type: String, required: true },
+    text: { type: String, required: true },
 });
 
 // Define the schema for Parasha
 const NewParashaSchema = new Schema({
-    author: { type: String, required: false },
-    image: { type: imageSchema, required: false },
-    title: { type: String, required: false },
-    miniText: { type: String, required: false },
-    parashPage: { type: [ParashPageSchema], required: false },
+    author: { type: String, required: true },
+    image: { type: imageSchema, required: true },
+    title: { type: String, required: true },
+    miniText: { type: String, required: true },
+    parashPage: { type: [ParashPageSchema], required: true },
 });
 
 export default NewParashaSchema;
