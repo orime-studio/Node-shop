1d716a44c2485fbb8b20f83c337baa3c7ede3898
TamamTamar <tamar.tamam770@gmail.com>
2024-11-19 12:55:26 +0200

    h

--- src/services/new-parasha-service.ts
+++ src/services/new-parasha-service.ts
@@ -2,12 +2,9 @@ import { parasha } from "../@types/chabad";
 import NewParasha from "../db/models/parasha-new-model";
 
 export const parashaService = {
-  createParasha: async (data: parasha) => {
-    try {
-      const parasha = new NewParasha(data);
-      return await parasha.save();
-    } catch (error) {
-      throw new Error(`Failed to create a new Parasha: ${error.message}`);
-    }
-  },
+    createParasha: async (data: parasha) => {
+        const parasha = new NewParasha(data);
+    
+        return parasha.save();
+      },
 };
