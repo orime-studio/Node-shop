a2058d629d701f49e82799b70a8bdf974e1cc51a
TamamTamar <tamar.tamam770@gmail.com>
2024-11-21 14:33:13 +0200

    t

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use(cors());
 
 
 
-/* app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads'))); */
+ app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads'))); 
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/analytics", analyticsRouter);
