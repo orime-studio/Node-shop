dd75e2e8ef2bd9b839a3fb3124686ea12773b8e8
TamamTamar <tamar.tamam770@gmail.com>
2024-11-23 21:00:56 +0200

    j

--- src/middleware/uploads.ts
+++ src/middleware/uploads.ts
@@ -5,22 +5,17 @@ import path from "path";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(__dirname, "../public/uploads");
-    console.log("Checking upload path:", uploadPath);
 
     // יצירת התיקייה אם היא לא קיימת
     if (!fs.existsSync(uploadPath)) {
-      console.log("Upload path does not exist. Creating it...");
       fs.mkdirSync(uploadPath, { recursive: true });
-      console.log("Upload path created successfully.");
     } else {
-      console.log("Upload path already exists.");
     }
 
     cb(null, uploadPath); // תיקיית היעד
   },
   filename: (req, file, cb) => {
     const fileName = Date.now() + "-" + file.originalname;
-    console.log("Generating filename:", fileName);
     cb(null, fileName); // שם הקובץ
   }
 });
