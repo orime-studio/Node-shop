d93cc2f6f9c7d1b8edd2c26334c733745af37844
TamamTamar <tamar.tamam770@gmail.com>
2024-11-24 10:41:00 +0200

    e

--- src/middleware/uploads.ts
+++ src/middleware/uploads.ts
@@ -1,11 +1,28 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+// שם חדש לתיקייה
+const uploadDirectory = "public/new-uploads";
+
+// בדוק אם התיקייה קיימת, ואם לא - צור אותה
+if (!fs.existsSync(uploadDirectory)) {
+    console.log(`Directory ${uploadDirectory} does not exist. Creating it...`);
+    fs.mkdirSync(uploadDirectory, { recursive: true });
+} else {
+    console.log(`Directory ${uploadDirectory} already exists.`);
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, `public/uploads`);
+        // הגדרת תיקיית היעד להעלאת הקובץ
+        cb(null, uploadDirectory);
     },
     filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
+        // יצירת שם קובץ ייחודי עם timestamp
+        const uniqueFileName = `${Date.now()}-${file.originalname}`;
+        console.log(`Uploading file: ${uniqueFileName}`); // הדפסת שם הקובץ המועלה
+        cb(null, uniqueFileName);
     },
 });
 
