0813734be164911f2e4c031e4bc19d800f887e14
TamamTamar <tamar.tamam770@gmail.com>
2024-11-24 10:39:18 +0200

    x

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.use(cors());
 
 /* app.use('/uploads', express.static(path.join(__dirname, '..', 'public', 'uploads')));  // תיקיית הסטטיים שהוגדרה
  */
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+/* app.use("/uploads", express.static(path.join(__dirname, "uploads"))); */
 
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/products", productRouter);
@@ -48,7 +48,7 @@ app.use('/api/v1/pages', pageRoute);
 app.use('/api/v1/parasha', newParashaRouter);
 
 app.use(express.static("public"));
-app.use(express.static("uploads"));
+
 app.use(errorHandler);
 app.use(notFound);
 
