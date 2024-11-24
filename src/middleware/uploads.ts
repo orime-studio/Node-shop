5b0ab193181e425dbe819e919b97f7b505065734
TamamTamar <tamar.tamam770@gmail.com>
2024-11-24 11:41:48 +0200

    g

--- src/middleware/uploads.ts
+++ src/middleware/uploads.ts
@@ -2,23 +2,29 @@ import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 
-// בדיקה אם תיקיית uploads קיימת ואם לא, ניצור אותה
-const uploadDirectory = 'public/uploads';
+// הגדרת תיקיית העלאת קבצים
+const uploadDirectory = 'public';
+
+// אם התיקיה לא קיימת, ניצור אותה
 if (!fs.existsSync(uploadDirectory)) {
+    console.log("Creating upload directory...");
     fs.mkdirSync(uploadDirectory, { recursive: true });
+} else {
+    console.log("Upload directory already exists.");
 }
 
 // הגדרת Multer לשמירת קבצים בתיקיית uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
+        console.log("Setting destination for file upload...");
         cb(null, uploadDirectory);
     },
     filename: (req, file, cb) => {
+        console.log(`Uploading file: ${file.originalname}`);
         cb(null, `${Date.now()}-${file.originalname}`);
     },
 });
 
-// יצירת אובייקט Multer להעלאת קובץ אחד
 const upload = multer({ storage });
 
 export default upload;
